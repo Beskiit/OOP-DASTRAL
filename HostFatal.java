81b075fb3e9893e73ab057cfd92122d22797a981
Anthony Renz Limsipson Maranan <150760576+dngrnz@users.noreply.github.com>
2024-01-06 19:18:33 +0800

    HostFatal.java
    With log in or sign up

--- HostFatal.java
+++ HostFatal.java
@@ -1,4 +1,5 @@
 package DastralOOP;
+import Java.util.Scanner;
 
 public class HostFatal extends Overriding{
 	public static void main(String[]args) {
@@ -6,4 +7,24 @@ public class HostFatal extends Overriding{
 		
 		over.name();
 	}
+	{
+	Scanner scn = new Scanner(System.in);
+
+		System.out.println("Welcome!");
+		System.out.println ("Do you want to:");
+		System.out.println ("1. Log In");
+		System.out.println ("2. Sign up");
+		System.out.println ("Enter your choice (1 or 2):");
+
+		int choice = scn.nextInt();
+
+		if(choice == 1){
+			System.out.println("You chose to log in.");
+		}else if (choice == 2){
+			System.out.println("You chose to sign up.");
+		}else {
+			System.out.println("Invalid choice, Please enter 1 or 2 only.");
+		}
+	}
+}
 }
