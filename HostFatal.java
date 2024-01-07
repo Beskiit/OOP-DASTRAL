92f5f4bd61c9cc30f25fd0a6ad62ab135aaeb462
Anthony Renz Limsipson Maranan <150760576+dngrnz@users.noreply.github.com>
2024-01-07 14:51:00 +0800

    Update HostFatal.java

--- HostFatal.java
+++ HostFatal.java
@@ -8,12 +8,14 @@ public class HostFatal extends Overriding{
 		over.name();
         Scanner scn = new Scanner(System.in);
 		System.out.println ("Do you want to:");
-		System.out.println ("1. Log In");
-		System.out.println ("2. Sign up");
-		System.out.println ("Enter your choice (1 or 2):");
-
+		System.out.println ("1 - Log In");
+		System.out.println ("2 - Sign up");
+                over.line();
+		System.out.println ("Press(1 or 2):");
+                over.line();
+                System.out.println("Your answer: ");
 		int choice = scn.nextInt();
-
+                over.line();
 		if (choice == 1) {
                 login();
                 } else if (choice == 2) {
@@ -22,31 +24,70 @@ public class HostFatal extends Overriding{
                 System.out.println("Invalid choice, Please enter 1 or 2 only.");
         }      
 	}
-	public static void login() {
+        
+        
+        
+	public static void login() {	
+        Overriding over = new Overriding();
         Scanner scn = new Scanner(System.in);
-        System.out.println("Enter your username: ");
+        over.line();
+        System.out.print("Enter your username: ");
         String username = scn.nextLine();
-        System.out.println("Enter your password: ");
+        
+        System.out.print("Enter your password: ");
         String password = scn.nextLine();
+        
 
         
         if (username.equals("admin") && password.equals("password")) {
             System.out.println("Login successful!");
-            
+            afterLogin();
         } else {
             System.out.println("Incorrect username or password.");
         }
     }
 	public static void signUp() {
+        Overriding over = new Overriding();
         Scanner scn = new Scanner(System.in);
-        System.out.println("Enter a new username: ");
+        System.out.print("Enter a new username: ");
         String newUsername = scn.nextLine();
-        System.out.println("Enter a new password: ");
+        over.line();
+        System.out.print("Enter a new password: ");
         String newPassword = scn.nextLine();
-
+        over.line();
         
         System.out.println("Account created successfully for username: " + newUsername);
         
     }
-	
+        public static void afterLogin() {
+        System.out.println("Welcome! You are now logged in.");
+
+        System.out.println("Select an option:");
+        System.out.println("1. View profile");
+        System.out.println("2. Modify settings");
+        System.out.println("3. Logout");
+
+        Scanner scanner = new Scanner(System.in);
+        int option = scanner.nextInt();
+
+        switch (option) {
+            case 1:
+                System.out.println("Viewing profile...");
+
+                break;
+            case 2:
+                System.out.println("Modifying settings...");
+
+                break;
+            case 3:
+                System.out.println("Logging out...");
+
+                break;
+            default:
+                System.out.println("Invalid option.");
+                break;
+        }
+    }
+        
+
 }
