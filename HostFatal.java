1455d83df8384b5ebadd149b29f40dd57bf6c255
Anthony Renz Limsipson Maranan <150760576+dngrnz@users.noreply.github.com>
2024-01-08 18:18:39 +0800

    Update HostFatal.java

--- HostFatal.java
+++ HostFatal.java
@@ -8,6 +8,10 @@ public class HostFatal extends Overriding{
 		
 		over.name();
 		loginProcess();
+		PatientBill pb = new PatientBill();
+        
+        pb.setAmount(3000.00);
+        System.out.println("Patient's Bill Amount: Php." + pb.getAmount());
 }      
         
         public static void loginProcess(){
