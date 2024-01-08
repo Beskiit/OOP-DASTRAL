54bde17c5f6934a8b4b356f3272b8fe0e4b8fd9b
Airone Gamil <119646968+Beskiit@users.noreply.github.com>
2024-01-08 21:34:13 +0800

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -1,14 +1,18 @@
 package hostfatal;
+import java.util.HashMap;
 import java.util.Scanner;
 
-public class Admin {
-	private static final String adminUsername = "Admin";
+public class Admin extends Overriding{
+	static Scanner sc = new Scanner(System.in);
+	static Overriding over = new Overriding();
+	static userSide patientInformation = new userSide();
+	static HashMap<String, String> username = new HashMap<String, String>();
+	private static final String adminUser = "admin";
 	
-	private static final String adminPassword = "Pogiako123";
+	private static final String adminPass = "admin";
 	
-	public void login(){
+	public static void adminLogin(){
 		boolean success = true;
-        Scanner sc = new Scanner(System.in);
         do {
         	System.out.print("Enter Admin Username : ");
         	String enterAdminUsername = sc.next();
@@ -26,71 +30,127 @@ public class Admin {
     }
       
       
-    private boolean isAdminValid(String adminUsername, String adminPassword) {
-    	return this.adminUsername.equals(adminUsername) && this.adminPassword.equals(adminPassword);
+    private static boolean isAdminValid(String adminUsername, String adminPassword) {
+    	return adminUser.equals(adminUsername) && adminPass.equals(adminPassword);
     }
+    
+    public static void startProgram() {
+    		System.out.println ("[1] - Admin");
+    		System.out.println ("[2] - User");
+    		System.out.print("Enter your choice: ");
+    		int choice = sc.nextInt();
+    	    over.line();
+    		if (choice == 1) {
+    	        adminLogin();
+    	    } else if (choice == 2) {
+    	        userLogin();
+    	    } else {
+    	    	System.out.println("Invalid choice, Please enter 1 or 2 only.");
+    	    }   
+    }
+    
+    public static void userLogin() {
+    	System.out.print("Enter username: ");
+    	String user = sc.next();
+    	System.out.print("Enter password: ");
+    	String pass = sc.next();
+    	
+    	if(username.containsKey(user) && username.containsValue(pass)) {
+    		userInside();
+    	}else {
+    		System.err.println("Username or password is incorrect! Please try again.");
+    	}
+    }
+    
+    public static void userInside() {
+    	boolean cont = true;
+    	while(cont) {
+	    	System.out.println("[1] Patients Health Record");
+	    	System.out.println("[2] Input new patient record");
+	    	System.out.println("[3] Edit patient record");
+	    	System.out.println("[4] Bills and payments");
+	    	System.out.println("[5] Exit");
+	    	int choice = sc.nextInt();
+	    		switch(choice) {
+	    		case 1:
+	    			patientInformation.healthRecord();
+	    			break;
+	    		case 2:
+	    			patientInformation.patientInfoFinal();
+	    			break;
+	    		case 3:
+	    			break;
+	    		case 4:
+	    			break;
+	    		case 5:
+	    			break;
+	    	}
+    }
+ }
 
-    private void adminActions() {
-        Scanner sc = new Scanner(System.in);
+    private static void adminActions() {
 
         int choice;
+        boolean cont = true;
         
-        do {
+        while(cont) {
             System.out.println("Admin Options");
-            System.out.println("1. Add Patient");
-            System.out.println("2. Remove Patient");
-            System.out.println("3. Register a Doctor ");
-            System.out.println("4. Remove a Doctor ");
-            System.out.println("5. List Register Doctor");
-            System.out.println("6. Exit");
+            System.out.println("[1] Add User");
+            System.out.println("[2] Remove User");
+            System.out.println("[3] View User");
+            System.out.println("[4] Exit");
             System.out.println("Enter your choice");
-            choice = sc.next().charAt(0);
+            choice = sc.nextInt();
            
             switch (choice) { 
                 case 1:
-                    addPatient();
+                    addUser();
                     break;
 
                 case 2:
-                    removePatient();
+                    removeUser();
                     break;
 
                 case 3:
+                	viewUser();
                     break;
 
                 case 4:
-                    break;
-
-                case 5:
+                	startProgram();
+                	cont = false;
                     break;
             }
-        } while (choice != 6);
+        }
 
     }
 
-    private void addPatient() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Patient name : ");
-        String patientName = sc.nextLine();
-        System.out.println("Enter Patient Contact : ");
-        String patientContact = sc.nextLine();
-        System.out.println("Patient added Name: " + patientName + ", Contact: " + patientContact);
+    private static void addUser() {
+        System.out.println("Enter Username: ");
+        String user = sc.next();
+        System.out.println("Enter Password: ");
+        String pass = sc.next();
+        username.put(user, pass);
+        System.out.println("Account created successfuly!");
+        over.line();
     }
 
-    private void removePatient() {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter Name Patient to Remove : ");
-        String namePatientToRemove = sc.nextLine();
-        System.out.println("Patient Removed Successfully");
+    private static void removeUser() {
+        System.out.print("Enter the username you want to remove: ");
+        String userRemove = sc.nextLine();
+        System.out.println("User" + userRemove + "Removed Successfully!");
+        username.remove(userRemove);
+        over.line();
     }
     
-    private void registerDoctor(){
-      
-    }
-    
-    private void removeDoctor(){
-        
+    private static void viewUser(){
+      if(username.isEmpty()) {
+    	  System.out.println("No accounts yet.");
+      }else {
+    	  for(String n : username.keySet()) {
+    		  System.out.println("\nUsername: " + n);
+    		  System.out.println("Password: " + n + "\n");
+    	  }
+    	  over.line();
+      }
     }
 }
-
-        
