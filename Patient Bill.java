6abf3df27d991e1a026e4e42b0d717b045a4d831
Anthony Renz Limsipson Maranan <150760576+dngrnz@users.noreply.github.com>
2024-01-08 19:02:15 +0800

    Update Patient Bill.java

--- Patient Bill.java
+++ Patient Bill.java
@@ -1,20 +1,27 @@
 public class PatientBill {
    private double totalAmount;
    private double amountPaid;
-   
+
+public PatientBill(){
+this.totalAmount = 0.0;
+this.amountPaid = 0.0;
+}   
+public PatientBill(double TotalAmount, double amountPaid){
+this.totalAmount = totalAmount;
+this.amountPaid = amountPaid;
+}
    public void setTotalAmount(double totalAmount){
-       this.totalAmount = totalAmount;
+    this.totalAmount = totalAmount;
    }
    public double getTotalAmount(){
-       return totalAmount;
+    return totalAmount;
    }
    public void setAmountPaid(double amountPaid){
-       this.amountPaid = amountPaid;
+    this.amountPaid = amountPaid;
    }
    public double getAmountPaid(){
-       return amountPaid;
+    return amountPaid;
    }
    public double calculateRemainingAmount(){
-       return totalAmount - amountPaid;
-   } 
-}
+    return totalAmount- amountPaid;
+   }
