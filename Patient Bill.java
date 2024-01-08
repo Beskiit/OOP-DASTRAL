ab3858db309b8d86c5d649ac5eaf714c091cba88
Anthony Renz Limsipson Maranan <150760576+dngrnz@users.noreply.github.com>
2024-01-08 18:47:29 +0800

    Update Patient Bill.java

--- Patient Bill.java
+++ Patient Bill.java
@@ -1,18 +1,20 @@
-public class PatientBill extends HostFatal{
-    private double amount;
-
-    public void setAmount(double amount){
-        this.amount = amount;
-    }
-public double getAmount(){
-        return amount;
-    }
-
-   public static void main(String[] args){
-        PatientBill pb = new PatientBill();
-        
-        pb.setAmount(3000.00);
-        System.out.println("Patient's Bill Amount: Php." + pb.getAmount());
-    }
-}     
-        
+public class PatientBill {
+   private double totalAmount;
+   private double amountPaid;
+   
+   public void setTotalAmount(double totalAmount){
+       this.totalAmount = totalAmount;
+   }
+   public double getTotalAmount(){
+       return totalAmount;
+   }
+   public void setAmountPaid(double amountPaid){
+       this.amountPaid = amountPaid;
+   }
+   public double getAmountPaid(){
+       return amountPaid;
+   }
+   public double calculateRemainingAmount(){
+       return totalAmount - amountPaid;
+   } 
+}
