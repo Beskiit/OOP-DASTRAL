b86d697b65c6b167dd0ad05822c934cff8916768
Airone Gamil <119646968+Beskiit@users.noreply.github.com>
2024-01-08 21:32:13 +0800

    Update Layout.java

--- Layout.java
+++ Layout.java
@@ -1,6 +1,8 @@
 package DastralOOP;
 
-public abstract class Layout {
+public abstract class Layout{
 	abstract void name();
 	abstract void line();
+	abstract void userLoginName();
+	abstract void admin();
 }
