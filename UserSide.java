c5cc2488733e8493c2663c66b2659fba151d2846
Airone Gamil <119646968+Beskiit@users.noreply.github.com>
2024-01-10 02:29:46 +0800

    Update UserSide.java

--- UserSide.java
+++ UserSide.java
@@ -1,51 +1,53 @@
 package DastralOOP;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.TreeMap;
 
-public class UserSide extends Admin {
+public class UserSide extends Admin{
     Map<String, List<String>> patientInfo = new TreeMap<>();
+    static Map<String, Double> patientBalances = new HashMap<>();
     int key = 1;
     Scanner sc = new Scanner(System.in);
 
     public void patientInfoFinal() {
+    	over.addPatientRecord();
         String patientKey = "PT - " + key;
         List<String> patientInformation = new ArrayList<>();
 
-        System.out.println("Enter the name of patient: ");
+        System.out.print("\t \t \t \t \t \t \t Enter the name of patient: ");
         patientInformation.add("Patient Name: " + sc.nextLine());
-        System.out.println("Enter the age of patient: ");
+        System.out.print("\t \t \t \t \t \t \t Enter the age of patient: ");
         patientInformation.add("Patient Age: " + sc.nextLine());
-        System.out.println("Enter the gender of patient: ");
+        System.out.print("\t \t \t \t \t \t \t Enter the gender of patient: ");
         patientInformation.add("Patient Gender: " + sc.nextLine());
-        System.out.println("Enter the address of patient: ");
+        System.out.print("\t \t \t \t \t \t \t Enter the address of patient: ");
         patientInformation.add("Patient Address: " + sc.nextLine());
-        System.out.println("Enter the room of patient: ");
+        System.out.print("\t \t \t \t \t \t \t Enter the room of patient: ");
         patientInformation.add("Patient Room: " + sc.nextLine());
-        System.out.println("Enter the illness of patient: ");
+        System.out.print("\t \t \t \t \t \t \t Enter the illness of patient: ");
         patientInformation.add("Patient Illness: " + sc.nextLine());
-        System.out.println("Enter the doctor of patient: ");
+        System.out.print("\t \t \t \t \t \t \t Enter the doctor of patient: ");
         patientInformation.add("Patient Doctor: " + sc.nextLine());
-        System.out.println("Enter the status of patient: ");
+        System.out.print("\t \t \t \t \t \t \t Enter the status of patient: ");
         patientInformation.add("Patient Status: " + sc.nextLine());
 
         patientInfo.put(patientKey, patientInformation);
+        patientBalances.put(patientKey, 0.0);
 
         key++;
-        System.out.println("Patient input done!");
-        over.line();
+        System.out.println("\t \t \t \t \t \t \t Patient input done!");
     }
 
     public void healthRecord(){
+    	over.viewPatientRecord();
         if (patientInfo.isEmpty()) {
-            System.out.println("No patient yet.");
-            over.line();
+            System.out.println("\t \t \t \t \t \t \t No patient yet.");
         }else{
-            printMap(patientInfo);
-            over.line();
+            printPatient(patientInfo);
         }
     }
     
@@ -54,54 +56,65 @@ public class UserSide extends Admin {
     }
     
     public void editKeyValues(Map<String, List<String>> map){
-        System.out.print("Enter the patient ID you want to edit: ");
+    	over.editPatientRecord();
+        System.out.print("\t \t \t \t \t \t \t Enter the patient ID you want to edit: ");
         String editPatient = sc.nextLine();
-        
+        over.line();
         if(map.containsKey(editPatient)){
-            System.out.println("Current patient information of " + editPatient + ":");
+            System.out.println("\t \t \t \t \t \t \t Current patient information of " + editPatient + ":");
             for (String value : map.get(editPatient)) {
-                System.out.println("  " + value);
+                System.out.println("\t \t \t \t \t \t " + value);
             }
-            
-            System.out.println("Enter the new patient information for " + editPatient);
+            over.line();
+            System.out.println("\t \t \t \t \t \t \t Enter the new patient information for " + editPatient);
             List<String> newPatientInfo = new ArrayList<>();
             
-            System.out.println("Enter the name of patient: ");
+            System.out.print("\t \t \t \t \t \t \t Enter the name of patient: ");
             newPatientInfo.add("Patient Name: " + sc.nextLine());
-            System.out.println("Enter the age of patient: ");
+            System.out.print("\t \t \t \t \t \t \t Enter the age of patient: ");
             newPatientInfo.add("Patient Age: " + sc.nextLine());
-            System.out.println("Enter the gender of patient: ");
+            System.out.print("\t \t \t \t \t \t \t Enter the gender of patient: ");
             newPatientInfo.add("Patient Gender: " + sc.nextLine());
-            System.out.println("Enter the address of patient: ");
+            System.out.print("\t \t \t \t \t \t \t Enter the address of patient: ");
             newPatientInfo.add("Patient Address: " + sc.nextLine());
-            System.out.println("Enter the room of patient: ");
+            System.out.print("\t \t \t \t \t \t \t Enter the room of patient: ");
             newPatientInfo.add("Patient Room: " + sc.nextLine());
-            System.out.println("Enter the illness of patient: ");
+            System.out.print("\t \t \t \t \t \t \t Enter the illness of patient: ");
             newPatientInfo.add("Patient Illness: " + sc.nextLine());
-            System.out.println("Enter the doctor of patient: ");
+            System.out.print("\t \t \t \t \t \t \t Enter the doctor of patient: ");
             newPatientInfo.add("Patient Doctor: " + sc.nextLine());
-            System.out.println("Enter the status of patient: ");
+            System.out.print("\t \t \t \t \t \t \t Enter the status of patient: ");
             newPatientInfo.add("Patient Status: " + sc.nextLine());
             
             map.put(editPatient, newPatientInfo);
 
-            System.out.println("The patient information has been updated!");
+            System.out.println("\t \t \t \t \t \t \t The patient information has been updated!");
+            over.line();
         } else {
-            System.out.println("Key not found. Please enter a valid key.");
+            System.out.println("\t \t \t \t \t \t \t Key not found. Please enter a valid key.");
         }
     }
 
-    public void printMap(Map<String, List<String>> map){
-        System.out.println("Current Patient: " + (key-1) + "\n");
+    public void printPatient(Map<String, List<String>> map){
+        System.out.println("\t \t \t \t \t \t \t Current Patient: " + (key-1) + "\n");
 
         for(Map.Entry<String, List<String>> entry : map.entrySet()){
             String key = entry.getKey();
             List<String> values = entry.getValue();
             over.line();
-            System.out.println("  Patiend ID: " + key);
+            System.out.println("\t \t \t \t \t \t \t Patient ID: " + key);
             for (String value : values) {
-                System.out.println("  " + value);
+                System.out.println("\t \t \t \t \t \t \t " + value);
             }
+        }
+    }
+    
+    public static void viewBalances() {
+        System.out.println("\t \t \t \t \t \t \t Patient Balances:");
+        for (Map.Entry<String, Double> entry : patientBalances.entrySet()) {
+            String patientID = entry.getKey();
+            Double balance = entry.getValue();
+            System.out.println("\t \t \t \t \t \t \t Patient ID: " + patientID + ", Balance: " + balance);
         }
     }
 }
