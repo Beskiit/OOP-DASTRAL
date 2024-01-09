89faa6a44350dc3e705d740044989a9bff46c6b8
Airone Gamil <119646968+Beskiit@users.noreply.github.com>
2024-01-09 22:32:38 +0800

    Update Layout.java

--- Layout.java
+++ Layout.java
@@ -5,4 +5,12 @@ public abstract class Layout{
 	abstract void line();
 	abstract void userLoginName();
 	abstract void admin();
+	abstract void adminOptions();
+	abstract void userOptions();
+	abstract void addPatient();
+	abstract void editPatient();
+	abstract void viewPatient();
+	abstract void doctors();
+	abstract void viewDoctors();
+	abstract void addDoctor();
 }
